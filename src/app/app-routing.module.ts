e3e45aabb6b5c5f11d40e8aba2fb3fc4e3466c62
Welbert Soares <139386609+Welbert-Soares@users.noreply.github.com>
2024-08-02 20:33:18 -0300

    configurando rotas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,25 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { CriarPensamentoComponent } from './components/pensamentos/criar-pensamento/criar-pensamento.component';
+import { ListarPensamentoComponent } from './components/pensamentos/listar-pensamento/listar-pensamento.component';
+
+const routes: Routes = [
+  {
+    path: "",
+    redirectTo: "listarPensamento",
+    pathMatch: "full" //full: redireciona se a rota for exatamente igual a vazia
+  },
+  {
+    path: "criarPensamento",
+    component: CriarPensamentoComponent
+  },
+  {
+    path: "litarPensamento",
+    component: ListarPensamentoComponent
+  },
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
